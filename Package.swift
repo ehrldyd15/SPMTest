2f962dc4eb3fc508086b732608ab93d043b9bede
kiyong <dky0979@gmail.com>
2023-08-30 14:16:27 +0900

    no message

--- Package.swift
+++ Package.swift
@@ -21,9 +21,24 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MetSPMTest",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .process("Resources")
+            ]),
         .testTarget(
             name: "MetSPMTestTests",
             dependencies: ["MetSPMTest"]),
     ]
 )
+
+
+/*
+리소스를 추가할 때, .process는 리소스 번들 루트에 위치되고, .copy는 디렉터리 구조를 유지하면서 추가됩니다.
+resources: [
+    .process("이미지파일 이름"),
+    .copy("Data")
+]
+
+타겟에서 리소스 번들에 접근할 때 SwiftPM은 Bundle.module이라는 accessor로 접근합니다.
+let image = UIImage(named: "Logo", in: Bundle.module)
+*/
--- Sources/MetSPMTest/MetButtons.swift
+++ Sources/MetSPMTest/MetButtons.swift
@@ -19,6 +19,7 @@ extension MetSPMTest {
 
 extension MetSPMTest.MetButtons {
     
+    @available(macOS 12.0, *)
     public struct BottomButton: View {
         private var handler: () -> ()
         
@@ -30,14 +31,52 @@ extension MetSPMTest.MetButtons {
         }
         
         public var body: some View {
-            Button() {
-                print("버튼 눌렀다")
-                handler()
-            } label: {
-                Text(text)
+            VStack {
+                Button() {
+                    print("버튼 눌렀다")
+                    handler()
+                } label: {
+                    Text(text)
+                }
+                .frame(width: 200, height: 50)
+                .foregroundColor(.white)
+                .background(.red)
+
+                Image("BG", bundle: Bundle.module2)
+                    .frame(width: 200, height: 300)
             }
         }
         
     }
+    
+}
+
+import class Foundation.Bundle
+
+private class BundleFinder {}
+
+extension Foundation.Bundle {
+
+    static var module2: Bundle = {
+        let bundleName = "MetSPMTest_MetSPMTest"
+
+        let candidates = [
+            Bundle.main.resourceURL,
+
+            Bundle(for: BundleFinder.self).resourceURL,
+
+            Bundle.main.bundleURL,
+        ]
+
+        for candidate in candidates {
+            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
+            
+            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
+                return bundle
+            }
+        }
+        
+        fatalError("unable to find bundle named BioSwift_BioSwift")
+    }()
     
 }
