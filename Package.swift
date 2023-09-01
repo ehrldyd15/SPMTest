8aec58351643716e7807cd31f9744cd4e54d4346
kiyong <dky0979@gmail.com>
2023-09-01 09:32:31 +0900

    no message

--- Package.swift
+++ Package.swift
@@ -21,7 +21,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MetSPMTest",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .process("Resources")
+            ]),
         .testTarget(
             name: "MetSPMTestTests",
             dependencies: ["MetSPMTest"]),
--- Sources/MetSPMTest/MetButtons.swift
+++ Sources/MetSPMTest/MetButtons.swift
@@ -20,6 +20,7 @@ extension MetSPMTest {
 extension MetSPMTest.MetButtons {
     
     @available(macOS 12.0, *)
+    @available(iOS 15.0, *)
     public struct BottomButton: View {
         private var handler: () -> ()
         
@@ -42,41 +43,11 @@ extension MetSPMTest.MetButtons {
                 .foregroundColor(.white)
                 .background(.red)
 
-                Image("BG")
+                Image("BG", bundle: Bundle.module)
                     .frame(width: 200, height: 300)
             }
         }
         
     }
-    
-}
-
-import class Foundation.Bundle
-
-private class BundleFinder {}
-
-extension Foundation.Bundle {
-
-    static var module2: Bundle = {
-        let bundleName = "MetSPMTest_MetSPMTest"
-
-        let candidates = [
-            Bundle.main.resourceURL,
-
-            Bundle(for: BundleFinder.self).resourceURL,
-
-            Bundle.main.bundleURL,
-        ]
-
-        for candidate in candidates {
-            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
-            
-            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
-                return bundle
-            }
-        }
-        
-        fatalError("unable to find bundle named BioSwift_BioSwift")
-    }()
     
 }
